22142c9b44fbca32430e9f19b25eaa82a78a75fc
Ronan-Codes <galv.ronan@gmail.com>
2021-09-18 23:04:01 -0400

    Query and resolvers in progress

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,21 @@
+const { User, Thought } = require('../models');
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
+        // thoughts: async() => {
+        //     return Thought.find().sort({ createdAt: -1 });
+        // }
+        thoughts: async(parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
         }
     }
 };
+// parent is more of a placeholder parameter, to access username argument from second parameter
+// We use a ternary operator `?` to check if username exists. If it does, we set params to an object with a username key set to that value. 
+    // If it doesn't, we simply return an empty object.
+    // If there's not data in params object, we simply return every thought
+// 4 arguments for resolvers: parent, args, context(logged-in status/API access token), info(operation's current state)
+
 
 module.exports = resolvers;
